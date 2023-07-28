c6bd0b7e7726816702c6ae972ce04af6aab448f5
Lucas Lima <lucas_sdl@hotmail.com>
2023-07-28 19:08:23 +0100

    69. Creating project Secret word

---
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h2>Secret word</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ 6_SECRET_WORD/secretword/src/data/word.js
@@ -0,0 +1,8 @@
+export const wordlist = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    computador: ["Mouse" , "Teclado", "Monitor", "Gabinete"],
+    programacão: ["Linguagem", "Framework", "Javascript", "React"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+}
