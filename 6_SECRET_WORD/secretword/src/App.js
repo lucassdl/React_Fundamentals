25c4cc1ca3591f35d3892b98a6dd3bcb4caddf16
Lucas Lima <lucas_sdl@hotmail.com>
2023-07-31 14:52:03 +0100

    74. Switching game stages

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -18,18 +18,30 @@ const stages = [
   {id: 3, name: "end"}
 ]
 
-
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
-  console.log(words);
+  // starts the secrets word game
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  }
+
+  // process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  }
+
+  // restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  }
 
   return (
     <div className="App">
-      {gameStage === "start" && <StartScreen />}
-      {gameStage === "game" && <Game />}
-      {gameStage === "end" && <GameOver />}
+      {gameStage === "start" && <StartScreen startGame={startGame}/>}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry}/>}
     </div>
   );
 }
--- 6_SECRET_WORD/secretword/src/components/GameOver.js
+++ 6_SECRET_WORD/secretword/src/components/GameOver.js
@@ -1,8 +1,11 @@
 import './GameOver.css';
 
-const GameOver = () => {
+const GameOver = ({retry}) => {
   return (
-    <div>GameOver</div>
+    <div>
+         <h1>Game over</h1>
+        <button onClick={retry}>Resetar jogo</button>
+    </div>
   )
 }
 
