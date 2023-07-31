82f701f9fe85b6a04addfacc5af9a14cca948b19
Lucas Lima <lucas_sdl@hotmail.com>
2023-07-31 14:18:42 +0100

    Class 71. Creating project components

--- 6_SECRET_WORD/secretword/src/data/words.js
+++ 6_SECRET_WORD/secretword/src/data/words.js
@@ -1,4 +1,4 @@
-export const wordlist = {
+export const wordsList = {
     carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
     fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
     corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
