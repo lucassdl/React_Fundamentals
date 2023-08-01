5588dfc3a2ecbac0bf9183cfbc421fc906f8faf8
Lucas Lima <lucas_sdl@hotmail.com>
2023-08-01 15:05:16 +0100

    87. Retrieving api data

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [products, setProducts] = useState([]);
+  const url = "http://localhost:3000/products";
+
+  // 1- resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      setProducts(data);
+    }
+    fetchData();
+  }, []);
+
+  console.log(products)
+
   return (
     <div className="App">
-     <h1>Lista de Produtos</h1>
+      <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - {product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
