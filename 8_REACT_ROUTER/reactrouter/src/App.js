3cc58f6f05bc9111cd8318ce9f0e8cdeb96be026
Lucas Lima <lucas_sdl@hotmail.com>
2023-08-16 20:08:18 +0100

    108. Url redirect

--- 8_REACT_ROUTER/reactrouter/src/App.js
+++ 8_REACT_ROUTER/reactrouter/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 //1 - config react router
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 //components
 import Navbar from './components/Navbar';
@@ -33,6 +33,8 @@ function App() {
           <Route path='/product/:id/info' element={<Info />} />
           {/* 9- search */}
           <Route path='/search' element={<Search />} />
+          {/* 10- redirect */}
+          <Route path='/company' element={<Navigate to='/about' />} />
           {/* 7- no match route */}
           <Route path='*' element={<NotFound />} />
         </Routes>
