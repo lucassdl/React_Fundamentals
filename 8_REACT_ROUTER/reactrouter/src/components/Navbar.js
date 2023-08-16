b09fc07191c43ed33302c458b7984ed493b87dea
Lucas Lima <lucas_sdl@hotmail.com>
2023-08-16 14:34:11 +0100

    100. Navigation between pages

---
+++ 8_REACT_ROUTER/reactrouter/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+    </nav>
+  )
+}
+
+export default Navbar
